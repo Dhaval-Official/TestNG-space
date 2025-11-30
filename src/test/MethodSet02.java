70d593c3831bdbe6e8a59c861e54b46ba407b9e2
Dhaval <dhavallakhaniofficial@gmail.com>
2025-11-30 20:59:46 +0530

    Committing TestNG codes

---
+++ src/test/MethodSet02.java
@@ -0,0 +1,17 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class MethodSet02 {
+
+	//Extra method Set
+	@Test
+	public void S38() {
+		System.out.println("This is S38 Method");
+	}
+	
+	@Test
+	public void S38_1() {
+		System.out.println("This is S38 2nd Method");
+	}
+}
---
+++ src/test/MethodSet03.java
@@ -0,0 +1,21 @@
+package test;
+
+import org.testng.annotations.Test;
+
+public class MethodSet03 {
+
+	@Test
+	public void S41_WebLogin() {
+		System.out.println("This is Web Login");
+	}
+	
+	@Test
+	public void S41_APILogin() {
+		System.out.println("This is API Login");
+	}
+	
+	@Test
+	public void S41_MobLogin() {			//this one need to be removed in S41
+		System.out.println("This is Mob Login");
+	}
+}

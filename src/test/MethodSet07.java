4c79b8fc24bd5c911ab75b6dc5f175116df60fa8
Dhaval <dhavallakhaniofficial@gmail.com>
2025-12-07 22:40:09 +0530

    Commit S48(U12.80) Basic Groups

---
+++ src/test/MethodSet07.java
@@ -0,0 +1,20 @@
+package test;
+
+public class MethodSet07 {
+
+	public void S48_1() {
+		System.out.println("This is S48 Method 7.1");
+	}
+	
+	public void S48_2() {
+		System.out.println("This is S48 Method 7.2");
+	}
+	
+	public void S48_3() {
+		System.out.println("This is S48 Method 7.2");
+	}
+	
+	public void S48_4() {
+		System.out.println("This is S48 Method 7.4");
+	}
+}
---
+++ src/test/MethodSet08.java
@@ -0,0 +1,20 @@
+package test;
+
+public class MethodSet08 {
+
+	public void S48_1() {
+		System.out.println("This is S48 Method 8.1");
+	}
+	
+	public void S48_2() {
+		System.out.println("This is S48 Method 8.2");
+	}
+	
+	public void S48_3() {
+		System.out.println("This is S48 Method 8.2");
+	}
+	
+	public void S48_4() {
+		System.out.println("This is S48 Method 8.4");
+	}
+}
